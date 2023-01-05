7b59dde97a5e548db4ab4b6e65075d6412d8c478
Alvaro Sivila <alvaro.sivila@jala.university>
2023-01-05 17:51:42 -0400

    Added test for checking the fields of the response json and negative test

--- src/test/java/org/tasks/LoginTest.java
+++ src/test/java/org/tasks/LoginTest.java
@@ -2,14 +2,18 @@ package org.tasks;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.apache.http.HttpStatus;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginTest {
     @Test
     public void loginTest(){
         String BASE_URL = "https://test-api.k6.io/auth/basic/login/";
-        String response = RestAssured
+        Response response = RestAssured
                 .given()
                 .contentType(ContentType.JSON)
                 .body("{\n" +
@@ -18,8 +22,52 @@ public class LoginTest {
                         "}")
                 .post(BASE_URL)
                 .then()
-                .statusCode(HttpStatus.SC_OK).toString();
-               //.body("json", notNullValue()).toString();
+                .assertThat().statusCode(200).extract().response();
+
+    }
+    @Test
+    public void invalidLogin(){
+        String BASE_URL = "https://test-api.k6.io/auth/basic/login/";
+        Response response = RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"username\": \"junior0123\",\n" +
+                        "    \"password\": \"test123222\"\n" +
+                        "}")
+                .post(BASE_URL)
+                .then()
+                .assertThat().statusCode(400).extract().response();
+
+    }
+    @Test
+    public void shouldCorrectData(){
+        String BASE_URL = "https://test-api.k6.io/auth/basic/login/";
+        int expectedUserID = 664436;
+        String expectedFirstName =  "Alvaro";
+        String expectedLastName = "Sivila";
+        String expectedEmail = "alvaro.sivila@jala.university";
+        Response response = RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .body("{\n" +
+                        "    \"username\": \"junior0123\",\n" +
+                        "    \"password\": \"test123\"\n" +
+                        "}")
+                .post(BASE_URL)
+                .then()
+                .assertThat().statusCode(200).extract().response();
+
+        JsonPath responseData = response.jsonPath();
+        int idValue = responseData.get("id");
+        String nameValue = responseData.get("first_name");
+        String lastNameValue = responseData.get("last_name");
+        String emailValue = responseData.get("email");
+        assertEquals(expectedFirstName,nameValue);
+        assertEquals(expectedLastName,lastNameValue);
+        assertEquals(expectedEmail,emailValue);
+
+
     }
 
 }
